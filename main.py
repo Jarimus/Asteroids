8c3a5e46cbea4e5d69ffd5d0b26367c01635f671
Jarimus <jari.n.ahonen@gmail.com>
2025-01-15 03:42:59 +0200

    add player polygon

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
+
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #initialization
     pygame.init()
     screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+    time = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    #main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return
             
 
         screen.fill( (0,0,0) )
+        player.draw(screen)
 
 
         pygame.display.flip()
+        dt = time.tick(FPS) / 1000
 
 
 if __name__ == "__main__":
